13707a89fedc585be3a10a693a4fb1631e7b25c9
Pham Huu Doi <doipham1812@gmail.con>
2026-04-09 18:01:29 +0700

    debug

--- server/src/config/prisma.js
+++ server/src/config/prisma.js
@@ -4,20 +4,20 @@ const { PrismaMariaDb } = require('@prisma/adapter-mariadb'); // adapter cho Mar
 const { URL } = require('node:url');
 
 if (!process.env.DATABASE_URL) {
-    throw new Error('Missing DATABASE_URL in .env');
+  throw new Error('Missing DATABASE_URL in .env');
 }
 
 const databaseUrl = new URL(process.env.DATABASE_URL);
 
 const prisma = new PrismaClient({
-    adapter: new PrismaMariaDb({
-        host: databaseUrl.hostname,
-        port: Number(databaseUrl.port || 3306),
-        user: decodeURIComponent(databaseUrl.username),
-        password: decodeURIComponent(databaseUrl.password),
-        database: databaseUrl.pathname.replace(/^\//, ''),
-    }),
-    log: ['error', 'warn', 'info'],
+  adapter: new PrismaMariaDb({
+    host: databaseUrl.hostname,
+    port: Number(databaseUrl.port || 3306),
+    user: decodeURIComponent(databaseUrl.username),
+    password: decodeURIComponent(databaseUrl.password),
+    database: databaseUrl.pathname.replace(/^\//, ''),
+  }),
+  log: ['error', 'warn', 'info'],
 });
 
 module.exports = prisma;
